78329253d4506322467570f02c51ba2eeae4419a
Firescar96 <nchinda2@gmail.com>
2015-11-18 22:43:43 -0500

    order of operations is annoying

--- client/macweb.js
+++ client/macweb.js
@@ -1,5 +1,4 @@
-Router.route('/(.*)[^\.]', {name: 'macweb'});
-Router.route('/', {name: 'macweb'});
+Router.route('/(.*)', {name: 'macweb'});
 
 Template.macweb.rendered = function () {
   $('img[usemap]').maphilight()
