919eee3993d0809ba78ac3e1470415a090b37b11
Firescar96 <nchinda2@gmail.com>
2016-08-15 18:02:41 -0500

    storage policy

--- client/react/Housecomm.js
+++ client/react/Housecomm.js
@@ -20,32 +20,32 @@ class Housecomm extends TrackerReact(Component) {
 
     return (
       <div>
-      <Navbar />
-      <div className="container">
+        <Navbar />
+        <div className="container">
           <div className="panel-group" id="accordion">
 
             <div className="panel panel-default">
               <div className="panel-heading">
                 <h2 className="panel-title">
                   <a className="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#housecomm-whos-who">
-                    Who's Who
+                    Who&#39;s Who
                   </a>
                 </h2>
               </div>
               <div id="housecomm-whos-who" className="panel-body panel-collapse collapse out">
                 <div className="panel-body">
-                <table className="table table-condensed table-striped">
-                  <thead>
-                    <tr>
-                      <th>Position</th>
-                      <th>Person</th>
-                      <th>Email</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {housecomm}
-                  </tbody>
-                </table>
+                  <table className="table table-condensed table-striped">
+                    <thead>
+                      <tr>
+                        <th>Position</th>
+                        <th>Person</th>
+                        <th>Email</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {housecomm}
+                    </tbody>
+                  </table>
                   <p>Entry chair emails are ichairs@mit.edu, where i is the entry letter you wish to contact.</p>
                 </div>
               </div>
@@ -64,6 +64,7 @@ class Housecomm extends TrackerReact(Component) {
                   <p><a  href="/housecomm-docs/constitution.pdf" target="_blank">Constitution</a></p>
                   <p><a href="/housecomm-docs/bylaws.pdf" target="_blank">Bylaws</a></p>
                   <p><a href="/housecomm-docs/security.pdf" target="_blank">Security Policy</a></p>
+                  <p><a href="/housecomm-docs/storage.pdf" target="_blank">Storage Policy</a></p>
                 </div>
               </div>
             </div>
@@ -80,34 +81,34 @@ class Housecomm extends TrackerReact(Component) {
                 <div className="panel-body">
                   <h2>Reimbursement</h2>
 
-      				    <p>These instructions are modified slightly from <a href="http://studentlife.mit.edu/sao/finance/training">SAO's Financial Training Page</a>. Please refer to that page if you have any questions. If you still have questions, email <a href="mailta:mac-trez@mit.edu">mac-trez@mit.edu</a>.</p>
+                  <p>These instructions are modified slightly from <a href="http://studentlife.mit.edu/sao/finance/training">SAO&#39;s Financial Training Page</a>. Please refer to that page if you have any questions. If you still have questions, email <a href="mailta:mac-trez@mit.edu">mac-trez@mit.edu</a>.</p>
 
-      				      <h3>Instructions</h3>
+                  <h3>Instructions</h3>
 
-      				      <ol start="0">
-      				        <li>Make sure to save your receipts!</li>
-      				        <li>Go to the <a href="https://atlas.mit.edu/atlas/Home.action#purchasing_1">Atlas Buying page</a>, and click on the Reimbursement link under <code>Requests for Payment (RFPs)</code></li>
-      				        <li>Fill out the RFP. Detailed instructions below.</li>
-      				        <li>Send the RFP to the Treasurer (no longer Jeremy Kaplan)</li>
-      				        <li>Print out the page displayed after sending</li>
-      				        <li>Staple your receipt(s) confirmation page you just printed. Take that to desk, and tell them to deliver it to the Treasurer.</li>
-      				        <li>[Optional] Nag the Treasurer until you are reimbursed.</li>
-      				      </ol>
+                  <ol start="0">
+                    <li>Make sure to save your receipts!</li>
+                    <li>Go to the <a href="https://atlas.mit.edu/atlas/Home.action#purchasing_1">Atlas Buying page</a>, and click on the Reimbursement link under <code>Requests for Payment (RFPs)</code></li>
+                    <li>Fill out the RFP. Detailed instructions below.</li>
+                    <li>Send the RFP to the Treasurer (no longer Jeremy Kaplan)</li>
+                    <li>Print out the page displayed after sending</li>
+                    <li>Staple your receipt(s) confirmation page you just printed. Take that to desk, and tell them to deliver it to the Treasurer.</li>
+                    <li>[Optional] Nag the Treasurer until you are reimbursed.</li>
+                  </ol>
 
-      				      <h3>RFP Instructions</h3>
-      				      <ol start="0">
-      				        <li>Follow the above instructions up to Step 2.</li>
-      				        <li>Enter the Payee (person being reimbursed) in the box, hit <code>Search</code>, and click on the result that matches you.</li>
-      				        <li>[Optional] Name the RFP anything you want (you can use it to search the system for it).</li>
-      				        <li>Fill out the <code>Date of Service</code> and <code>Amount</code> fields with their respective values. (Remember, DO NOT INCLUDE TAX.)</li>
-      				        <li>DO NOT INCLUDE TAX!</li>
-      				        <li>Fill the <code>G/L</code> field using the reference at the bottom of <a href="http://studentlife.mit.edu/sao/finance/faq">this page</a></li>
-      				        <li>Fill the <code>Cost Object</code> field with <code>2720608</code> (MacGregor's account number)</li>
-      				        <li>Fill the <code>Explanation</code> field appropriately</li>
-      				        <li>Click <code>Save &amp; Continue</code>.</li>
-      				        <li>Scan and attach your receipt(s) to the RFP you've created.</li>
-      				        <li>Enter the Treasurer's name in the box (Aaron Zeng), hit <code>Search</code>, and click on the result that matches the Treasurer (kerberos: a2z)</li>
-      				      </ol>
+                  <h3>RFP Instructions</h3>
+                  <ol start="0">
+                    <li>Follow the above instructions up to Step 2.</li>
+                    <li>Enter the Payee (person being reimbursed) in the box, hit <code>Search</code>, and click on the result that matches you.</li>
+                    <li>[Optional] Name the RFP anything you want (you can use it to search the system for it).</li>
+                    <li>Fill out the <code>Date of Service</code> and <code>Amount</code> fields with their respective values. (Remember, DO NOT INCLUDE TAX.)</li>
+                    <li>DO NOT INCLUDE TAX!</li>
+                    <li>Fill the <code>G/L</code> field using the reference at the bottom of <a href="http://studentlife.mit.edu/sao/finance/faq">this page</a></li>
+                    <li>Fill the <code>Cost Object</code> field with <code>2720608</code> (MacGregor&#39;s account number)</li>
+                    <li>Fill the <code>Explanation</code> field appropriately</li>
+                    <li>Click <code>Save &amp; Continue</code>.</li>
+                    <li>Scan and attach your receipt(s) to the RFP you&#39;ve created.</li>
+                    <li>Enter the Treasurer&#39;s name in the box (Aaron Zeng), hit <code>Search</code>, and click on the result that matches the Treasurer (kerberos: a2z)</li>
+                  </ol>
                 </div>
               </div>
             </div>
