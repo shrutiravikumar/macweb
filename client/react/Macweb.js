76c01f806cd49849b927b0d2876b05bbb1418dae
Firescar96 <nchinda2@gmail.com>
2017-08-21 10:35:12 -0400

    create new h-entry page with acheng98

--- client/react/Macweb.js
+++ client/react/Macweb.js
@@ -15,7 +15,7 @@ class Macweb extends Component {
         <area href="/entry-pages/e-entry.pdf" shape="poly" coords="285,270,329,295,402,254,418,244,402,254,402,304,329,346,285,321" alt="Link" data-maphilight='{"strokeColor":"100505","strokeWidth":4,"fillColor":"100505","fillOpacity":1}' title="E Entry" />
         <area href="/entry-pages/f-entry.pdf" shape="poly" coords="402,254,446,228,593,313,593,381,520,423,520,355,475,330,447,346,402,322" alt="Link" data-maphilight='{"strokeColor":"800080","strokeWidth":4,"fillColor":"800080","fillOpacity":1}' title="F Entry" />
         <area href="/entry/g" shape="poly" coords="475,330,520,355,520,423,329,533,329,466,285,440" alt="Link" data-maphilight='{"strokeColor":"e25822","strokeWidth":4,"fillColor":"e25822","fillOpacity":1}' title="G Entry" />
-        <area href="/entry-pages/h-entry.pdf" shape="poly" coords="285,440,329,466,329,533,256,575,138,508,138,440,182,414,256,457" alt="Link" data-maphilight='{"strokeColor":"0aa200","strokeWidth":4,"fillColor":"0aa200","fillOpacity":1}' title="H Entry" />
+        <area href="/entry/h/h.html" shape="poly" coords="285,440,329,466,329,533,256,575,138,508,138,440,182,414,256,457" alt="Link" data-maphilight='{"strokeColor":"0aa200","strokeWidth":4,"fillColor":"0aa200","fillOpacity":1}' title="H Entry" />
         <area href="/entry/j" shape="poly" coords="182,414,138,440,138,508,65,465,65,398,152,347,196,372,196,422" alt="Link" data-maphilight='{"strokeColor":"8c41d8","strokeWidth":4,"fillColor":"8c41d8","fillOpacity":1}' title="J Entry" />
       </map>
     )
