28c2b75133c3a96f6643db128a45f42a0a84f352
ed1d1a8d <twang6@mit.edu>
2017-09-22 16:37:34 -0400

    Remove inline css

--- client/react/Navbar.js
+++ client/react/Navbar.js
@@ -6,55 +6,40 @@ class Navbar extends Component {
       <nav className="navbar navbar-static-top" role="navigation">
         <div className="container-fluid">
           <div className="navbar-header">
-            <a className="navbar-brand" href="/" style={{ outline: 0 }}>
+            <a className="navbar-brand no-outline" href="/">
               MacGregor
             </a>
           </div>
 
           <ul className="nav navbar-nav">
             <li id="nav-housecomm">
-              <a href="/housecomm" style={{ outline: 0 }}>
-                Administrivia
-              </a>
+              <a href="/housecomm">Administrivia</a>
             </li>
             <li id="nav-support">
-              <a href="/rex" style={{ outline: 0 }}>
-                REX
-              </a>
+              <a href="/rex">REX</a>
             </li>
             <li id="nav-support">
-              <a href="/support" style={{ outline: 0 }}>
-                Student Support
-              </a>
+              <a href="/support">Student Support</a>
             </li>
             <li id="nav-maccon">
-              <a href="/maccon" style={{ outline: 0 }}>
-                MacCon
-              </a>
+              <a href="/maccon">MacCon</a>
             </li>
             <li id="nav-desk">
-              <a href="/desk" style={{ outline: 0 }}>
-                Movie List
-              </a>
+              <a href="/desk">Movie List</a>
             </li>
             <li id="nav-laundry">
               <a
                 href="http://laundryview.com/laundry_room.php?lr=1364815"
                 target="_blank"
-                style={{ outline: 0 }}
               >
                 Laundry
               </a>
             </li>
             <li id="nav-dininghall">
-              <a href="/dininghall" style={{ outline: 0 }}>
-                Dining Hall
-              </a>
+              <a href="/dininghall">Dining Hall</a>
             </li>
             <li id="nav-about">
-              <a href="/about" style={{ outline: 0 }}>
-                About
-              </a>
+              <a href="/about">About</a>
             </li>
           </ul>
         </div>
