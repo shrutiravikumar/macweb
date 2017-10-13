80097597ecf3a30aa0b5609c1457f005241638d9
ed1d1a8d <twang6@mit.edu>
2017-10-13 02:28:00 -0400

    Update support

--- client/react/Support.js
+++ client/react/Support.js
@@ -1,56 +1,76 @@
-import React, { Component } from 'react';
-import TrackerReact from 'meteor/ultimatejs:tracker-react';
-import Navbar from './Navbar.js';
+import React, { Component } from "react";
+import TrackerReact from "meteor/ultimatejs:tracker-react";
+import Navbar from "./Navbar.js";
 
-Meteor.subscribe('supports');
+Meteor.subscribe("supports");
 const Supports = new Mongo.Collection("supports");
-Meteor.subscribe('medlinks');
+Meteor.subscribe("medlinks");
 const MedLinks = new Mongo.Collection("medlinks");
-Meteor.subscribe('peerears');
+Meteor.subscribe("peerears");
 const PeerEars = new Mongo.Collection("peerears");
 
 class Support extends TrackerReact(Component) {
   render() {
+    let sources = Supports.find({})
+      .fetch()
+      .map(support => {
+        return (
+          <tr key={support.name}>
+            <td>{support.role}</td>
+            <td>{support.name}</td>
+            <td>{support.contact}</td>
+          </tr>
+        );
+      });
 
-    let sources = Supports.find({}).fetch().map((support) => {
-      return (
-        <tr key={support.name}>
-          <td>{support.role}</td>
-          <td>{support.name}</td>
-          <td>{support.contact}</td>
-        </tr>
-      );
-    });
+    let medlinks = MedLinks.find({})
+      .fetch()
+      .map(medlink => {
+        return (
+          <tr key={medlink.name}>
+            <td>{medlink.name}</td>
+            <td>{medlink.email}</td>
+            <td>{medlink.entry}</td>
+            <td>{medlink.phone}</td>
+          </tr>
+        );
+      });
 
-    let medlinks = MedLinks.find({}).fetch().map((medlink) => {
-      return (
-      <tr key={medlink.name}>
-        <td>{medlink.name}</td>
-        <td>{medlink.email}</td>
-        <td>{medlink.entry}</td>
-        <td>{medlink.phone}</td>
-      </tr>
-    );
-    });
-
-    let peerears = PeerEars.find({}).fetch().map((peerEar) => {
-      return (
-        <tr key={peerEar.name}>
-          <td>{peerEar.name}</td>
-          <td>{peerEar.entry}</td>
-          <td>{peerEar.email}</td>
-        </tr>
-      )
-    });
+    let peerears = PeerEars.find({})
+      .fetch()
+      .map(peerEar => {
+        return (
+          <tr key={peerEar.name}>
+            <td>{peerEar.name}</td>
+            <td>{peerEar.entry}</td>
+            <td>{peerEar.email}</td>
+          </tr>
+        );
+      });
 
     return (
       <div>
         <Navbar />
         <div id="support" className="container">
-          <h2>Student Support</h2>
+          <p>
+            Life can be tough sometimes. This page lists some resources at MIT
+            that can help you.
+          </p>
 
-          <p>There are a lot of people who you can get support from. If you aren't sure where to start you can always default to classmates and your GRT. Always.</p>
-          <br/>
+          <p>
+            In addition to the contents below, more resources are listed in this{" "}
+            <a href="/support-resources.pdf" target="blank_">
+              resource list curated by UA Student Support and Wellness
+            </a>.
+          </p>
+
+          <h3>Uncategorized Individuals</h3>
+
+          <p>
+            Here is a list of some people you can ask for help. If you aren't
+            sure where to start, try your classmates and GRTs first.
+          </p>
+          <br />
 
           <table className="table table-condensed table-striped">
             <thead>
@@ -60,16 +80,23 @@ class Support extends TrackerReact(Component) {
                 <th>Contact Info</th>
               </tr>
             </thead>
-            <tbody>
-              {sources}
-            </tbody>
+            <tbody>{sources}</tbody>
           </table>
-          <br/>
+          <br />
 
           <h3>Medlinks</h3>
 
-          <p>MedLinks can answer questions about MIT Medical’s policies and services and can help you figure out when and how to connect with other MIT resources. They also provide single doses of common over-the-counter medications, first-aid materials, and safer-sex supplies. <a href="https://medlinks.mit.edu/where-we-are?field_residence_value%5B%5D=dorm_macg">Medlink Site</a></p>
-          <br/>
+          <p>
+            MedLinks can answer questions about MIT Medical’s policies and
+            services and can help you figure out when and how to connect with
+            other MIT resources. They also provide single doses of common
+            over-the-counter medications, first-aid materials, and safer-sex
+            supplies.{" "}
+            <a href="https://medlinks.mit.edu/where-we-are?field_residence_value%5B%5D=dorm_macg">
+              Medlink Site
+            </a>
+          </p>
+          <br />
 
           <table className="table table-condensed table-striped">
             <thead>
@@ -80,16 +107,21 @@ class Support extends TrackerReact(Component) {
                 <th>Phone Number</th>
               </tr>
             </thead>
-            <tbody>
-              {medlinks}
-            </tbody>
+            <tbody>{medlinks}</tbody>
           </table>
 
-          <br/>
+          <br />
           <h3>Peer Ears</h3>
 
-          <p>Peer Ears are here to support mental health and answer any questions about MIT’s mental health resources. They are trained and confidential, and most importantly, they’re peers. Feel free to drop a note or come talk to them, and if you want to join to add more support for MacGregor or get help now, you can find out more on their website, mitpeerears.org.</p>
-          <br/>
+          <p>
+            Peer Ears are here to support mental health and answer any questions
+            about MIT’s mental health resources. They are trained and
+            confidential, and most importantly, they’re peers. Feel free to drop
+            a note or come talk to them, and if you want to join to add more
+            support for MacGregor or get help now, you can find out more on
+            their website, mitpeerears.org.
+          </p>
+          <br />
 
           <table className="table table-condensed table-striped">
             <thead>
@@ -99,11 +131,8 @@ class Support extends TrackerReact(Component) {
                 <th>Email</th>
               </tr>
             </thead>
-            <tbody>
-              {peerears}
-            </tbody>
+            <tbody>{peerears}</tbody>
           </table>
-
         </div>
       </div>
     );
