d1a31a4f18cf7971d3595877d5c3438a696855b9
ed1d1a8d <twang6@mit.edu>
2017-10-17 23:19:43 -0400

    Add google analytics

--- client/react/main.js
+++ client/react/main.js
@@ -8,6 +8,29 @@ import Desk from "./Desk.js";
 import About from "./About.js";
 import DiningHall from "./DiningHall";
 
+(function(i, s, o, g, r, a, m) {
+  i["GoogleAnalyticsObject"] = r;
+  (i[r] =
+    i[r] ||
+    function() {
+      (i[r].q = i[r].q || []).push(arguments);
+    }),
+    (i[r].l = 1 * new Date());
+  (a = s.createElement(o)), (m = s.getElementsByTagName(o)[0]);
+  a.async = 1;
+  a.src = g;
+  m.parentNode.insertBefore(a, m);
+})(
+  window,
+  document,
+  "script",
+  "https://www.google-analytics.com/analytics.js",
+  "ga"
+);
+
+ga("create", "UA-66853834-1", "auto");
+ga("send", "pageview");
+
 // Routes for site navigation
 FlowRouter.route("/", {
   action: function(params, queryParams) {
