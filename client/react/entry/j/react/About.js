f0999efd759f39515dc47780b78e247aa941fd4d
ed1d1a8d <twang6@mit.edu>
2018-01-28 22:49:42 -0500

    Update jentry about and traditions

--- client/react/entry/j/react/About.js
+++ client/react/entry/j/react/About.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import Navbar from './Navbar.js';
+import React, { Component } from "react";
+import Navbar from "./Navbar.js";
 
 class About extends Component {
   render() {
@@ -13,28 +13,56 @@ class About extends Component {
 
         <div className="container-fluid">
           <div className="row">
-            <div className="col-md-2 col-sm-1 col-xs-1"></div>
+            <div className="col-md-2 col-sm-1 col-xs-1" />
             <div className="col-md-8 col-sm-10 col-xs-10">
-              <img id="main-photo" className="img-responsive" src="/entry/j/yearbook_progressive.jpg"/>
+              <img
+                id="main-photo"
+                className="img-responsive"
+                src="/entry/j/yearbook.jpg"
+              />
             </div>
-            <div className="col-md-2 col-sm-1 col-xs-1"></div>
+            <div className="col-md-2 col-sm-1 col-xs-1" />
           </div>
 
           <div className="row">
-            <div className="col-md-2 col-sm-1 col-xs-1"></div>
-            <div id="entry-description" className="col-md-8 col-sm-10 col-xs-10">
+            <div className="col-md-2 col-sm-1 col-xs-1" />
+            <div
+              id="entry-description"
+              className="col-md-8 col-sm-10 col-xs-10"
+            >
               <p>
-                J Entry is a friendly and welcoming place where you can always find people to hang out with. We often leave our doors open so that people can stop to talk as they pass by, and there are always people in our lounges p-setting, procrastinating, or just hanging out (or sometimes all of them at once!) in the evenings and until the wee hours of the morning. As one of the smallest entries in MacGregor, we are very close-knit, which provides us not only with a fun place to hang out with friends in our free time, but also a great support network anytime we’re having trouble with something. We try to create a friendly, caring, family-like atmosphere that makes J Entry a place where you can relax, be yourself, and feel at home. J Entry is a place where you can come to have a great time with fun people, but also one where you can find peace and quiet when you need it.
+                J Entry is a friendly and welcoming place where you can always
+                find people to hang out with. We often leave our doors open so
+                that people can stop to talk as they pass by, and there are
+                always people in our lounges p-setting, procrastinating, or just
+                hanging out (or sometimes all of them at once!) in the evenings
+                and until the wee hours of the morning. As one of the smallest
+                entries in MacGregor, we are very close-knit, which provides us
+                not only with a fun place to hang out with friends in our free
+                time, but also a great support network anytime we’re having
+                trouble with something. We try to create a friendly, caring,
+                family-like atmosphere that makes J Entry a place where you can
+                relax, be yourself, and feel at home. J Entry is a place where
+                you can come to have a great time with fun people, but also one
+                where you can find peace and quiet when you need it.
               </p>
               <p>
-                We have weekly study breaks here in J, at which we celebrate the weekend by taking time out to have fun together! Study break traditions include our Thanksgiving potluck, J-Idol, a bowling trip, restaurant outings, tie-dyeing, a hiking trip, our holiday gift exchange, and the Easter piñata, just to name a few. We also have weekly(-ish) brownie nights to take a break during the week, and the magical Birthday Fairy makes us cakes to celebrate our birthdays! Other things our residents enjoy together are movies, food, puzzles, board games, and staying up late hanging out in the lounge.
+                We have weekly study breaks here in J, at which we celebrate the
+                weekend by taking time out to have fun together! Study break
+                traditions include our Thanksgiving potluck, teaching, a bowling
+                trip, food nights, karaoke, a hiking trip, and our holiday gift
+                exchange, just to name a few. Other things our residents enjoy
+                together are movies, food, puzzles, board games, and staying up
+                late hanging out in the lounge.
               </p>
-              <br/>
-              <a href="/entry/j/residents">Meet some of our fantastic residents!</a>
-              <br/>
-              <br/>
+              <br />
+              <a href="/entry/j/residents">
+                Meet some of our fantastic residents!
+              </a>
+              <br />
+              <br />
             </div>
-            <div className="col-md-2 col-sm-1 col-xs-1"></div>
+            <div className="col-md-2 col-sm-1 col-xs-1" />
           </div>
         </div>
       </div>
--- client/react/entry/j/react/Traditions.js
+++ client/react/entry/j/react/Traditions.js
@@ -1,78 +1,147 @@
-import React, { Component } from 'react';
-import Navbar from './Navbar.js';
+import React, { Component } from "react";
+import Navbar from "./Navbar.js";
 
 class Traditions extends Component {
   render() {
     return (
       <div className="entryj">
         <Navbar />
-          <div className="title-div row">
-            <div className="col-md-2"></div>
-            <div className="col-md-8">
-              <h1>Our J Entry Traditions</h1>
-              <p>
-                In J we like to try all sorts of things. A lot of what we do is spontaneous, unique and unreproducible!
-                However, we also have some traditions that we try to keep, and we'd like to share some of those with you here.
-              </p>
-            </div>
-            <div className="col-md-2"></div>
+        <div className="title-div row">
+          <div className="col-md-2" />
+          <div className="col-md-8">
+            <h1>Our J Entry Traditions</h1>
           </div>
+          <div className="col-md-2" />
+        </div>
 
-          <div className="row">
-            <div className="col-md-2"></div>
-            <div id="entry-description" className="col-md-8">
-              <h3>Lounge-Dwelling</h3>
-              <p>
-                In true MIT fashion, we (or at least some of us) like to procrastinate! So rather than focus on our work alone in our lovely singles we sometimes work in lounges just for the company and casual goofing around. Occasionally we find ourselves distracted...
-              </p>
+        <div className="row">
+          <div className="col-md-2" />
+          <div id="entry-description" className="col-md-8">
+            <p>
+              In J we like to try all sorts of things. A lot of what we do is
+              spontaneous, unique and unreproducible! However, we also have some
+              traditions that we try to keep, and we'd like to share some of
+              those with you here.
+            </p>
 
-              <h3>Trolling and Pranks</h3>
-              <p>
-                Have you ever heard the troll song originally sung by Eduard Khil? Well, don’t worry if you haven’t, because you’ll hear plenty of it when you live here! We love playing pranks on each other, no matter how silly they might be. Be forewarned! Troll or be trolled in this entry! And FYI, if you really do love the troll action, be sure to check out J2 for the daily Ron-talks-to-Kevin show that never fails to amuse.
-              </p>
+            <h4>Lounge-Dwelling</h4>
+            <p>
+              In true MIT fashion, we (or at least some of us) like to
+              procrastinate! So rather than focus on our work alone in our
+              lovely singles we sometimes work in lounges just for the company
+              and casual goofing around. Occasionally we find ourselves
+              distracted...
+            </p>
 
-              <h3>Thanksgiving Potluck</h3>
-              <p>
-                Because Thanksgiving is all ‘bout that food!! Every year we each make some small dish or something for Thanksgiving and then get together to try and eat it all (we always fail). But hey, leftovers are good too, right? :D
-              </p>
+            <h4>Trolling and Pranks</h4>
+            <p>
+              Have you ever heard the troll song originally sung by Eduard Khil?
+              Well, don’t worry if you haven’t, because you’ll hear plenty of it
+              when you live here! We love playing pranks on each other, no
+              matter how silly they might be. Be forewarned! Troll or be trolled
+              in this entry!
+            </p>
 
-              <h3>Winter Gift Exchange</h3>
-              <p>
-                Right before Christmas time we have a gift exchange (white elephant/yankee swap type thing) within J. People buy some silly gifts and have a ball trying to take the best ones for ourselves (for some definitions of best).
-              </p>
+            <h4>Jamily Dinner</h4>
+            <p>
+              Members of J Entry gather every night to eat dinner together at a
+              dining hall. Some are on meal plans, others just use tech cash.
+              Round tables at Next can always accommodate n+1 people. Current
+              record is 17 people!
+            </p>
 
-              <h3>Senior Dinner</h3>
-              <p>
-                Every year we like to dress up fancy pancy and go out to some restaurant to enjoy a nice dinner with our entry’s seniors as a sort-of farewell. We usually try to wrap up the night by spending time talking about (usually funny and embarrassing) memories/moments with the seniors.
-              </p>
+            <h4>Thanksgiving Potluck</h4>
+            <p>
+              Because Thanksgiving is all ‘bout that food!! Every year we each
+              make some small dish or something for Thanksgiving and then get
+              together to try and eat it all (we always fail). But hey,
+              leftovers are good too, right?
+            </p>
 
-              <h3>Stressed Desserts</h3>
-              <p>
-                When under stress, one of the first impulses is to pig out on junk food and anything that offers you a delicious taste at maximum cost to your personal health, so why not do it with friends? Once a semester, when we sense that the stress is getting high due to a ton of assignments/tests, we have a “Stressed Desserts” study break (stressed = desserts spelled backwards :D) where we can all pig out together and vent out all of our academic and personal frustrations at the world and MIT.
-              </p>
+            <h4>Winter Gift Exchange</h4>
+            <p>
+              Right before Christmas time we have a gift exchange (white
+              elephant/yankee swap type thing) within J. People buy some silly
+              gifts and have a ball trying to take the best ones for ourselves
+              (for some definitions of best).
+            </p>
 
-              <h3>Bad Puns</h3>
-              <p>
-                They say laughter is great way to handle stress, and at in J, we spend a lot of time laughing. Jokes are a great way to do so, but as we are engineers and not liberal arts majors, our jokes can often come across as knee-slappers or super nerdy (don’t worry, once you start hanging out with people in your major, those puns will really take off). Sometimes, we have those awkward moments when almost everyone misses the joke. But ultimately, bad or good, awkward or not awkward, we love to make puns: they’re a great way to break out of the monotony that is PSets and classes.
-              </p>
+            <h4>Senior Dinner</h4>
+            <p>
+              Every year we like to dress up fancy and go out to some restaurant
+              to enjoy a nice dinner with our entry’s seniors as a sort-of
+              farewell. We usually try to wrap up the night by spending time
+              talking about (usually funny and embarrassing) memories/moments
+              with the seniors.
+            </p>
 
-              <h3>Bowling</h3>
-              <p>
-                Every semester or so we venture out to the wilderness known as Alewife (actually, just the end of the Red Line) and compete for eternal glory. The game? Bowling.
-              </p>
+            <h4>Spontaneous Food Trips</h4>
+            <p>
+              Sometimes people want to eat things late at night. Does it matter
+              that IHOP is all the way over by Harvard? Of course not! These
+              late night trips often go to IHOP, the South Street Diner, or Top
+              of the Hub.
+            </p>
 
-              <h3>Brownie Night</h3>
-              <p>
-                Every week or so in the evening, we chow down on some delicious, fresh-baked brownies. It serves as a great break between studying/work, and everyone has a good time.
-              </p>
+            <h4>Bad Puns</h4>
+            <p>
+              They say laughter is great way to handle stress, and at in J, we
+              spend a lot of time laughing. Jokes are a great way to do so, but
+              as we are engineers and not liberal arts majors, our jokes can
+              often come across as knee-slappers or super nerdy (don’t worry,
+              once you start hanging out with people in your major, those puns
+              will really take off). Sometimes, we have those awkward moments
+              when almost everyone misses the joke. But ultimately, bad or good,
+              awkward or not awkward, we love to make puns: they’re a great way
+              to break out of the monotony that is Psets and classes.
+            </p>
 
-              <h3>Finals Breakfasts</h3>
-              <p>
-                During finals week, our lovely GRTs provide us with breakfast so we have one less thing to worry about during this stressful time. It’s a great time to relax before exams, hang out, and wish each other the best of luck!
-              </p>
-            </div>
-            <div className="col-md-2"></div>
+            <h4>Door Signs</h4>
+            <p>
+              At the beginning of each year, door signs are made for everyone
+              that incorporates some aspect of their interests/personality.
+            </p>
+
+            <h4>JSWOLE</h4>
+            <p>
+              J Entry likes to be active when we can. Many people go rock
+              climbing. Others run. In order to facilitate a more active J, we
+              try to hold group workouts on Saturdays. All levels of experience
+              are welcome!
+            </p>
+
+            <h4>Murder Mystery</h4>
+            <p>
+              Each REX and CPW, our esteemed thespians (AKA anyone who can be
+              wrangled into acting) put on a Murder Mystery dinner for new
+              prefrosh. Each year features a harried race to The Garment
+              District for costumes, last-minute script and puzzle writing, and
+              poorly-timed costume changes after our fake blood inevitably
+              spills. When the freshmen arrive, a shot goes off, killing their
+              host. They then spend the rest of their dinner solving puzzles
+              (with heavy-handed help from us) to discover the killer and
+              motive.
+            </p>
+
+            <h4>Civil War</h4>
+            <p>
+              One of our entry symbols is a small Christmas tree that has been
+              in the entry for years. Every so often, the tree will be stolen by
+              rebels and must be recovered by entry loyalists. Battle typically
+              ensues, but a Tree-ty has been signed to secure the tree. Who
+              knows how long the peace will last...
+            </p>
+
+            <h4>Finals Breakfasts</h4>
+            <p>
+              During finals week, our lovely GRTs provide us with breakfast so
+              we have one less thing to worry about during this stressful time.
+              It’s a great time to relax before exams, hang out, and wish each
+              other the best of luck!
+            </p>
           </div>
+          <div className="col-md-2" />
+        </div>
       </div>
     );
   }
